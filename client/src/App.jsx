031428e2e1d5c3ee14d152d0be3711d1e4563f2f
Murat Altun <mraltun@gmail.com>
2022-09-24 12:21:07 +0300

    Added empty pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <h1>My Blog</h1>
-      <div id='page-body'>Hello there</div>
+      <div id='page-body'>
+        <HomePage />
+        <AboutPage />
+        <ArticlePage />
+        <ArticlesListPage />
+      </div>
     </div>
   );
 };
---
+++ client/src/pages/ArticlesListPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArticlesListPage = () => {
+  return <div>ArticlesListPage</div>;
+};
+
+export default ArticlesListPage;
