c448c4b68b25e582e3427ba6f96ee506a1935eb4
Murat Altun <mraltun@gmail.com>
2022-09-24 13:00:30 +0300

    Added article-content data and render it inside ArticlePage

--- client/src/pages/ArticlePage.jsx
+++ client/src/pages/ArticlePage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import articles from "../data/article-content";
 
 const ArticlePage = () => {
-  return <div>ArticlePage</div>;
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph) => (
+        <p key={paragraph}>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
