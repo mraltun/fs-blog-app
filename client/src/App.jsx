2bbd77311a8094e8d4806f1b169da14f76df8288
Murat Altun <mraltun@gmail.com>
2022-09-24 12:31:46 +0300

    Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>My Blog</h1>
-      <div id='page-body'>
-        <HomePage />
-        <AboutPage />
-        <ArticlePage />
-        <ArticlesListPage />
+    <BrowserRouter>
+      <div className='App'>
+        <h1>My Blog</h1>
+        <div id='page-body'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles' element={<ArticlesListPage />} />
+            <Route path='/articles/:articleId' element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
