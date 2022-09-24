662f765c8c194b0bd384a57aa5680f7b4af60ccc
Murat Altun <mraltun@gmail.com>
2022-09-24 13:39:31 +0300

    Moved Article List in separate component

--- client/src/pages/ArticlesListPage.jsx
+++ client/src/pages/ArticlesListPage.jsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
 import articles from "../data/article-content";
 
 const ArticlesListPage = () => {
   return (
     <>
       <h1>Articles</h1>
-      {articles.map((article) => (
-        <Link
-          to={`/articles/${article.name}`}
-          key={article.name}
-          className='article-list-item'
-        >
-          <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0, 150)}...</p>
-        </Link>
-      ))}
+      <ArticlesList articles={articles} />
     </>
   );
 };
