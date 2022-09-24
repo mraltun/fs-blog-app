0980871c3eed2c9a42ea769b754e4af1335ab518
Murat Altun <mraltun@gmail.com>
2022-09-24 16:03:36 +0300

    Implemented upvote route for articles

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,33 @@
 import express from "express";
 
+// Temp DB
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
+
 const app = express();
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-  res.send("Hello");
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((item) => item.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes}`);
+  } else {
+    res.send("That article does not exits");
+  }
 });
 
 app.listen(8000, () => {
