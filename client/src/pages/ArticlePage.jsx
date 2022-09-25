71b809a366f290532ee4cba563a3ef838a9a8e47
Murat Altun <mraltun@gmail.com>
2022-09-25 12:29:39 +0300

    Refactor ArticlePage to get data from the backend

--- client/src/pages/ArticlePage.jsx
+++ client/src/pages/ArticlePage.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "../data/article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios(
+        `http://localhost:8000/api/articles/${articleId}`
+      );
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+
+    loadArticleInfo();
+  }, []);
+
   const article = articles.find((article) => article.name === articleId);
 
   if (!article) {
@@ -14,6 +29,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)!</p>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
