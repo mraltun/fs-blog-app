c4f1ad70c111a9672332178c616e106e668eb431
Murat Altun <mraltun@gmail.com>
2022-09-25 17:31:49 +0300

    Added useUser custom hook

--- client/src/hooks/useUser.jsx
+++ client/src/hooks/useUser.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const useUser = () => {
-  return <div>useUser</div>;
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(getAuth(), (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+
+    return unsub;
+  }, []);
+
+  return { user, isLoading };
 };
 
 export default useUser;
--- client/src/pages/ArticlePage.jsx
+++ client/src/pages/ArticlePage.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import articles from "../data/article-content";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 import NotFoundPage from "./NotFoundPage";
+import articles from "../data/article-content";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+  const { user, isLoading } = useUser();
 
   useEffect(() => {
     const loadArticleInfo = async () => {
@@ -36,16 +38,24 @@ const ArticlePage = () => {
     <>
       <h1>{article.title}</h1>
       <div className='upvotes-section'>
-        <button onClick={addUpvote}>Upvote</button>
+        {user ? (
+          <button onClick={addUpvote}>Upvote</button>
+        ) : (
+          <button>Log in to upvote</button>
+        )}
         <p>This article has {articleInfo.upvotes} upvote(s)!</p>
       </div>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
-      <AddCommentForm
-        articleName={articleId}
-        onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
-      />
+      {user ? (
+        <AddCommentForm
+          articleName={articleId}
+          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+        />
+      ) : (
+        <button>Log in to add a comment</button>
+      )}
       <CommentsList comments={articleInfo.comments} />
     </>
   );
