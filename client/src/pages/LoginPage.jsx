17a29fb8089179b4c1b44d5bdcfc7ec52cd862dd
Murat Altun <mraltun@gmail.com>
2022-09-25 17:17:42 +0300

    Implemented LoginPage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const logIn = async () => {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate("/articles");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Log In</h1>
+      {error && <p className='error'>{error}</p>}
+      <input
+        placeholder='Your email address'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type='email'
+        name=''
+      />
+      <input
+        placeholder='Your password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type='password'
+        name=''
+      />
+      <button onClick={logIn}>Log In</button>
+      <Link to='/create-account'>Don't have an account? Create one here</Link>
+    </>
+  );
 };
 
 export default LoginPage;
