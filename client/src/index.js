ad3a133b4e57f7a71d745bcd2e473709f94b36f5
Murat Altun <mraltun@gmail.com>
2022-09-25 14:59:13 +0300

    Added Firebase to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyBK8wh_TdcDY8WgBMR5hNYz90QVnrOok5M",
+  authDomain: "fs-blog-app.firebaseapp.com",
+  projectId: "fs-blog-app",
+  storageBucket: "fs-blog-app.appspot.com",
+  messagingSenderId: "526669963893",
+  appId: "1:526669963893:web:0a2e637962fc39a5448e3c",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
